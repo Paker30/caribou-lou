0665fa74de8db90dec7fb672cf5cf2264ac76c93
Francisco Moreno Cantero <fmoreno@marcoaldany.com>
2018-03-08 22:56:08 +0100

    test: fix small test
    affects: caribou-lou-api

--- packages/caribou-lou-api/test/index.js
+++ packages/caribou-lou-api/test/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
+const { expect } = require('code');
 const Lab = require('lab');
-const { it, describe, expect } = exports.lab = Lab.script();
+const { it, describe, test } = exports.lab = Lab.script();
 
 //Section to test
 process.env.NODE_ENV = 'test';
 const hapiServer = require('../src/server');
 
-it('Check it\'s a hapi server', (done) => {
+it('Check it\'s a hapi server', () => {
 
   expect(hapiServer).to.exists();
-  done();
 });
